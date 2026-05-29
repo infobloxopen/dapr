474d6961052a80e0e6842e7b7906ec1aae211bd7
Pushp Raj <praj1.infoblox.com>
2026-05-29 18:30:47 +0530

    PTOPA-19966: bind dapr.io/http-port and dapr.io/public-port annotations

--- pkg/injector/patcher/sidecar.go
+++ pkg/injector/patcher/sidecar.go
@@ -90,8 +90,8 @@ type SidecarConfig struct {
 	ActorsService               string
 	RemindersService            string
 	SentrySPIFFEID              string
-	SidecarHTTPPort             int32 `default:"3500"`
-	SidecarPublicPort           int32 `default:"3501"`
+	SidecarHTTPPort             int32 `annotation:"dapr.io/http-port" default:"3500"`
+	SidecarPublicPort           int32 `annotation:"dapr.io/public-port" default:"3501"`
 
 	Enabled                             bool    `annotation:"dapr.io/enabled"`
 	AppPort                             int32   `annotation:"dapr.io/app-port"`
